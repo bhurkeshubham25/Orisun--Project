62efa42144373184d31a6e64af5c8d78f4feea9c
bhurkeshubham25 <bhurkeshubham25@gmail.com>
2025-10-06 06:16:23 +0530

    Deploy Orisun project from main branch

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Orisun--Project/', // 👈 IMPORTANT: Must match your repo name exactly
+  base: '/Orisun--Project/', // 👈 must match your GitHub repo name exactly
 })
